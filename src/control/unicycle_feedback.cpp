e0b8421b9b0c89310cfed1020daae0384fc3c9e4
Jon <jonathan.woolfrey@gmail.com>
2026-01-28 11:13:06 +1100

    Reworked for renamed Unicycle class. Tested and functional

---
+++ src/control/unicycle_feedback.cpp
@@ -0,0 +1,205 @@
+/**
+ * @file    unicycle_feedback.cpp
+ * @author  Jon Woolfrey
+ * @email   jonathan.woolfrey@gmail.com
+ * @date    January 2026
+ * @version 1.0
+ * @brief   Numerical simulation to test nonlinear feedback control of differential drive class.
+ * 
+ * @copyright Copyright (c) 2025 Jon Woolfrey
+ * 
+ * @license GNU General Public License V3
+ * 
+ * @see https://github.com/Woolfrey/software_robot_library for more information.
+ */
+ 
+#include <Eigen/Core>
+#include <fstream>                                                                                  // Reading and writing to files
+#include <iostream>  
+#include <RobotLibrary/Control/UnicycleFeedback.h>
+#include <RobotLibrary/Math/Ellipsoid.h>
+#include <RobotLibrary/Model/Pose2D.h>
+#include <RobotLibrary/Trajectory/MinimumArcLength.h>
+
+// Simulation parameters
+double controlFrequency  = 100;
+double simulationTime    = 10.0;
+
+int simulationSteps = 1000;
+
+int main(int argc, char **argv)
+{   
+    using namespace Eigen;
+    using namespace RobotLibrary;
+    
+    // Set up the trajectory
+    Model::Pose2D startPose(0.0, 0.0, 0.0);
+    Vector2d endPoint = {-1.0, 1.0};
+    Trajectory::MinimumArcLength trajectory(startPose, endPoint, 1.0, simulationTime - 1.0);
+    
+    // Parameters for the model
+    Model::UnicycleParameters modelParameters; 
+    
+    double density = 500.0;
+    double height  = 0.30;
+    double radius  = 0.25;
+    double mass    = M_PI * radius * radius * height * density;
+    
+    modelParameters.inertia                = mass * radius * radius / 2.0;                          // Rotational inertia (kg*m^2)
+    modelParameters.mass                   = mass;                                                  // Weight (kg)
+    modelParameters.maxAngularAcceleration = 0.5;                                                   // Maximum rotational acceleration (rad/s/s)
+    modelParameters.maxAngularVelocity     = 100.0 * M_PI / 30.0;                                   // Maximum rotational speed (rad/s)
+    modelParameters.maxLinearAcceleration  = 1.0;                                                   // Maximum forward acceleration (m/s/s)
+    modelParameters.maxLinearVelocity      = 2.0;                                                   // Maximum forward speed (m/s)
+    modelParameters.minimumSafeDistance    = 1e-08;                                                // Make it the same as the robot
+    modelParameters.propagationUncertainty = Matrix3d::Identity();                                  // Uncertainty of configuration propagation in Kalman filter
+    
+    // Parameters for the feedback controller
+    Control::UnicycleFeedbackParameters controlParameters;
+    
+    controlParameters.controlFrequency    = controlFrequency;
+    controlParameters.minimumSafeDistance = 1e-06;
+    controlParameters.orientationGain     = 10.0;
+    controlParameters.xPositionGain       = 20.0;
+    controlParameters.yPositionGain       = 40.0;
+    
+    controlParameters.qpSolver.stepSizeTolerance = 1e-04;                                           // Needs to be very small for this low dimensional problem
+
+    Control::UnicycleFeedback controller(modelParameters, controlParameters);                       // Here is where we actually create the controller
+    
+    // Set up the obstacles
+    double rx = 0.1;
+    double ry = 0.1;
+    Matrix2d shapeMatrix;
+    shapeMatrix << rx*rx, 0.0,
+                     0.0, ry*ry;                                                                    // Defines shape of ellipse
+    
+    std::vector<std::vector<Model::Obstacle2D>> obstacles(simulationSteps);                         // Container
+    
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        auto ellipse = std::make_unique<Math::Ellipse>(shapeMatrix);                                // Underlying shape
+        obstacles[i].push_back(Model::Obstacle2D(std::move(ellipse)));
+        obstacles[i].back().update_state(Model::Pose2D(-0.3, 0.75, 0.0));
+        obstacles[i].back().set_name("ellipse_01");
+    }
+    
+    // Set up data arrays
+    std::vector<Model::UnicycleState> desiredStates(simulationSteps);                                // For saving trajectory data
+    std::vector<Model::UnicycleState> actualStates(simulationSteps);
+    
+    // Start conditions
+    Model::Pose2D actualPose(0.0, 0.0, 0.0);                                                        // Start offset from the trajectory
+    Vector2d controlInput = {0.0, 0.0};
+    controller.update_state(actualPose, controlInput);
+
+    // Run the simulation
+    for (int i = 0; i < simulationSteps; ++i)
+    { 
+        double simTime = i / controlFrequency;
+        
+        // Query the desired state from the trajectory generator
+        const auto &[desiredConfiguration,
+                     desiredVelocity,
+                     desiredAcceleration] = trajectory.query_state(simTime);                        // Get the desired state
+                     
+        Model::Pose2D desiredPose(desiredConfiguration[0],
+                                  desiredConfiguration[1],
+                                  desiredConfiguration[2]);                                         // We need to put it in a Pose2D object
+        
+        // NOTE: QP solver can throw an error if no solution exists.
+        try
+        {  
+            controlInput = controller.track_trajectory(desiredPose, desiredVelocity, obstacles[i]);
+        }
+        catch (const std::exception &exception)
+        {
+            std::cout << exception.what() << "\n";
+            
+            break;
+        }
+        
+        // Save data for analysis
+        desiredStates[i] = Model::UnicycleState{desiredPose, desiredVelocity};
+        actualStates[i]  = Model::UnicycleState{actualPose, controlInput};
+
+        // Update for next loop
+        controller.update_state(actualPose, controlInput);
+        actualPose = controller.predicted_pose();                                                   // Assume perfect tracking
+    }
+    
+    std::ofstream file;
+
+    // Desired configuration
+    file.open("unicycle_desired_configuration.csv");
+    file << "time_s,x_position_m,y_position_m,heading_angle_rad\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << desiredStates[i].pose.translation()[0] << ",";
+        file << desiredStates[i].pose.translation()[1] << ",";
+        file << desiredStates[i].pose.angle() << "\n";
+    }
+    file.close();
+    
+    // Feedforward velocity
+    file.open("unicycle_desired_velocity.csv");
+    file << "time_s,linear_velocity_m_s,angular_velocity_rad_s\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << desiredStates[i].velocity[0] << ",";
+        file << desiredStates[i].velocity[1] << "\n";
+    }
+    file.close();
+    
+    // Actual configuration data
+    file.open("unicycle_actual_configuration.csv");
+    file << "time_s,x_position_m,y_position_m,heading_angle_rad\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << actualStates[i].pose.translation()[0] << ",";
+        file << actualStates[i].pose.translation()[1] << ",";
+        file << actualStates[i].pose.angle() << "\n";
+    }
+    file.close();
+    
+    // Control inputs
+    file.open("unicycle_control_inputs.csv");
+    file << "time_s,linear_velocity_m_s,angular_velocity_rad_s\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << actualStates[i].velocity[0] << ",";
+        file << actualStates[i].velocity[1] << "\n";
+    }
+    file.close();
+    
+    // Robot properties
+    file.open("unicycle_robot_properties.csv");
+    file << "robot_inertia,robot_mass,robot_radius\n";
+    file << modelParameters.inertia << ",";
+    file << mass << ",";
+    file << radius;
+    file.close();
+    
+    // Obstacle
+    file.open("unicycle_obstacle_data.csv");
+    file << "time_s,centre_x,centre_y,xx,xy,yy\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << obstacles[i][0].pose().translation()[0] << ",";
+        file << obstacles[i][0].pose().translation()[1] << ",";
+        file << shapeMatrix(0,0) << ",";
+        file << shapeMatrix(0,1) << ",";
+        file << shapeMatrix(1,1) << "\n";
+    }
+    file.close();
+
+    std::cout << "[INFO] [UNICYCLE FEEDBACK CONTROL] Numerical simulation complete. "
+              << "Data saved to .csv files for analysis.\n";
+    
+    return 0;                                                                                       // No problems with main
+}
---
+++ src/control/unicycle_predictive.cpp
@@ -0,0 +1,247 @@
+/**
+ * @file    unicycle_predictive.cpp
+ * @author  Jon Woolfrey
+ * @email   jonathan.woolfrey@gmail.com
+ * @date    January 2026
+ * @version 1.0
+ * @brief   Numerical simulation to test nonlinear feedback control of unicycle class.
+ * 
+ * @copyright Copyright (c) 2025 Jon Woolfrey
+ * 
+ * @license GNU General Public License V3
+ * 
+ * @see https://github.com/Woolfrey/software_robot_library for more information.
+ */
+ 
+#include <Eigen/Core>
+#include <fstream>                                                                                  // Reading and writing to files
+#include <iostream>  
+#include <RobotLibrary/Control/UnicyclePredictive.h>
+#include <RobotLibrary/Math/Line.h>
+#include <RobotLibrary/Model/Pose2D.h>
+#include <RobotLibrary/Trajectory/MinimumArcLength.h>
+
+// Simulation parameters
+double simulationTime   =  20.0;
+double controlFrequency = 100.0;
+
+unsigned int simulationSteps  = 2000;
+unsigned int predictionSteps  = 100;
+
+int main(int argc, char **argv)
+{   
+    using namespace Eigen;
+    using namespace RobotLibrary;
+    
+    // Set up the trajectory
+    Model::Pose2D startPose(0.0, 0.0, 0.0);
+    Vector2d endPoint = {-1.0, 1.0};
+    Trajectory::MinimumArcLength trajectory(startPose, endPoint, 1.0, simulationTime - 1.0);
+    
+    // Parameters for the model
+    Model::UnicycleParameters modelParameters; 
+    
+    double density = 500.0;
+    double height  = 0.30;
+    double radius  = 0.1;
+    double mass    = M_PI * radius * radius * height * density;
+    
+    modelParameters.inertia                = mass * radius * radius / 2.0;                          // Rotational inertia (kg*m^2)
+    modelParameters.mass                   = mass;                                                  // Weight (kg)
+    modelParameters.maxAngularAcceleration = 1.0;                                                   // Maximum rotational acceleration (rad/s/s)
+    modelParameters.maxAngularVelocity     = 200.0 * M_PI / 30.0;                                   // Maximum rotational speed (rad/s)
+    modelParameters.maxLinearAcceleration  = 1.0;                                                   // Maximum forward acceleration (m/s/s)
+    modelParameters.maxLinearVelocity      = 2.0;                                                   // Maximum forward speed (m/s)
+    modelParameters.minimumSafeDistance    = 1e-10;
+    modelParameters.propagationUncertainty = Matrix3d::Identity();                                  // Uncertainty of configuration propagation in Kalman filter
+    
+    // Parameters for the predictive controller
+    Control::UnicyclePredictiveParameters controlParameters;
+    controlParameters.controlFrequency        = controlFrequency;
+    controlParameters.exponent                = 1e-03;                                              // Growth or decay of pose error weighting
+    controlParameters.maximumControlStepNorm  = 1e-03;                                              // DDP algorithm terminates early if max. ||du|| is smaller than this
+    controlParameters.numberOfRecursions      = 50;                                                 // No. of forward & backward passes for the DDP algorithm
+    controlParameters.obstaclePotentialScalar = 5e-02;                                              // Scales the repulsion force
+    controlParameters.predictionSteps         = predictionSteps;                                    // Length of prediction horizon
+  
+    controlParameters.poseErrorWeight <<   1.0,    0.0,    0.0,
+                                           0.0,    1.0,  9e-06,
+                                           0.0,  9e-06,  1e-05;
+                                           
+    controlParameters.poseErrorWeight *= 1.0;
+
+    // Create the controller and set initial conditions
+    Control::UnicyclePredictive controller(modelParameters, controlParameters);
+    Model::Pose2D actualPose(0.0, 0.0, 0.0);                                                        // Start offset from the trajectory
+    Vector2d controlInput = {0.0, 0.0}; 
+    controller.update_state(actualPose, controlInput);
+    
+    // Set up obstacle(s)
+    std::vector<std::vector<std::vector<Model::Obstacle2D>>> obstacles(simulationSteps);
+    
+    double r_x = 0.10;
+    double r_y = 0.10;
+        
+    Matrix2d shapeMatrix;
+    shapeMatrix << r_x * r_x,       -0.0,
+                         -0.00, r_y * r_y; 
+                         
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        obstacles[i].resize(predictionSteps+1);                                                     // MUST be N+1; makes it easier to handle in code
+        
+        for (int j = 0; j <= predictionSteps; ++j)
+        {
+            for (int k = 0; k < 1; ++k)
+            {
+                auto ellipse = std::make_unique<Math::Ellipse>(shapeMatrix);
+                
+                obstacles[i][j].push_back(Model::Obstacle2D(std::move(ellipse)));
+                obstacles[i][j].back().update_state(Model::Pose2D(-0.3,0.8, 0.0));
+                obstacles[i][j].back().set_name("ellipse_" + std::to_string(k+1));
+            }
+        }
+    }
+                                       
+    // Set up data arrays
+    std::vector<std::vector<Model::UnicycleState>> desiredStates(simulationSteps);                  // For saving trajectory data
+    std::vector<std::vector<Model::UnicycleState>> predictedStates(simulationSteps);
+    
+    // Run the simulation
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        double simTime = i / controlFrequency;                                                      // Dividing is more numerically stable
+
+        for (int j = 0; j <= predictionSteps; ++j)
+        {
+            const auto &[pos, vel, acc] = trajectory.query_state(simTime + j / controlFrequency);   // Sample the trajectory across the horizon
+         
+            Model::UnicycleState state(Model::Pose2D(pos[0], pos[1], pos[2]), vel);                 // We need to put it in a data structure
+            
+            desiredStates[i].push_back(state);                                                      // Append
+        }
+
+        try
+        {
+            controlInput = controller.track_trajectory(desiredStates[i], obstacles[i]);             // Solve the predictive control problem
+        }
+        catch (const std::exception &exception)
+        {
+            throw std::runtime_error("[ERROR] [UNICYCLE PREDICTIVE CONTROL] "
+                                     "Failed to solve trajectory tracking on simulation step " + std::to_string(i+1) + ":\n"
+                                     + std::string(exception.what()));
+             
+             break;
+        }
+        
+        // Save data for analysis
+        predictedStates[i] = controller.predicted_states();
+
+        // For next loop
+        controller.update_state(actualPose, controlInput);
+        actualPose = controller.predicted_pose();                                                   // Propagate the state
+    }
+    
+    std::ofstream file;
+
+    // Desired configuration
+    file.open("unicycle_desired_configuration.csv");
+    file << "time_s,x_position_m,y_position_m,heading_angle_rad\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << desiredStates[i][0].pose.translation()[0] << ",";
+        file << desiredStates[i][0].pose.translation()[1] << ",";
+        file << desiredStates[i][0].pose.angle() << "\n";
+    }
+    file.close();
+
+    // Feedforward velocity
+    file.open("unicycle_desired_velocity.csv");
+    file << "time_s,linear_velocity_m_s,angular_velocity_rad_s\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << desiredStates[i][0].velocity[0] << ",";
+        file << desiredStates[i][0].velocity[1] << "\n";
+    }
+    file.close();
+  
+    // Actual configuration data
+    file.open("unicycle_actual_configuration.csv");
+    file << "time_s,x_position_m,y_position_m,heading_angle_rad\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << predictedStates[i][0].pose.translation()[0] << ",";
+        file << predictedStates[i][0].pose.translation()[1] << ",";
+        file << predictedStates[i][0].pose.angle() << "\n";
+    }
+    file.close();
+
+    // Control inputs
+    file.open("unicycle_control_inputs.csv");
+    file << "time_s,linear_velocity_m_s,angular_velocity_rad_s\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << predictedStates[i][0].velocity[0] << ",";
+        file << predictedStates[i][0].velocity[1] << "\n";
+    }
+    file.close();
+
+    // Robot properties
+    file.open("unicycle_robot_properties.csv");
+    file << "robot_inertia,robot_mass,robot_radius\n";
+    file << modelParameters.inertia << ",";
+    file << mass << ",";
+    file << radius;
+    file.close();
+
+    // Obstacle
+    file.open("unicycle_obstacle_data.csv");
+    file << "time_s,centre_x,centre_y,xx,xy,yy\n";
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        file << (double)(i / controlFrequency) << ",";
+        file << obstacles[i][0][0].pose().translation()[0] << ",";
+        file << obstacles[i][0][0].pose().translation()[1] << ",";
+        file << shapeMatrix(0,0) << ",";
+        file << shapeMatrix(0,1) << ",";
+        file << shapeMatrix(1,1) << "\n";
+    }
+    file.close();
+    
+    // Predicted x positions
+    file.open("unicycle_predicted_x.csv");
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        for (int j = 0; j <= predictionSteps; ++j)
+        {
+            file << predictedStates[i][j].pose.translation()[0];
+            
+            if (j < predictionSteps) file << ",";
+            else                     file << "\n";
+        }
+    }
+    file.close();
+    
+    // Predicted x
+    file.open("unicycle_predicted_y.csv");
+    for (int i = 0; i < simulationSteps; ++i)
+    {
+        for (int j = 0; j <= predictionSteps; ++j)
+        {
+            file << predictedStates[i][j].pose.translation()[1];
+            
+            if (j < predictionSteps) file << ",";
+            else                     file << "\n";
+        }
+    }
+    file.close();
+
+    std::cout << "[INFO] [UNICYCLE PREDICTIVE CONTROL] Numerical simulation complete. "
+              << "Data saved to .csv files for analysis.\n";
+    
+    return 0;                                                                                          // No problems with main
+}
